22559522746317759da123bf475f9323c6f5621c
ivtikss <kirika2002op@gmail.com>
2024-03-16 22:49:12 +0500

    Add Pagitator

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на странице
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то выдать первую страницу
+        posts = paginator.page(1)
     return render(request, 
                   'blog/post/list.html', 
                   {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, 
-                             id=id, 
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     
     return render(request, 
                   'blog/post/detail.html', 
