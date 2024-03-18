ab1403f7529d29cb2a2fc2b0864fbe368462f62f
ivtikss <kirika2002op@gmail.com>
2024-03-18 11:38:17 +0500

    Change def view to class

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Постраничная разбивка с 3 постами на странице
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то выдать последнюю страницу результатов
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то выдать первую страницу
-        posts = paginator.page(1)
-    return render(request, 
-                  'blog/post/list.html', 
-                  {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, 
